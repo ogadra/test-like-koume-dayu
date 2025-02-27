4a7316862f4764134722577e914e0f60554afa00
ygkn <ygkn@users.noreply.github.com>
2025-02-27 17:26:00 +0900

    表示を改善

--- koume.test.ts
+++ koume.test.ts
@@ -1,9 +1,74 @@
 import { expect, test } from "vitest";
 
-test("toBeLikeKomeDayu", () => {
-  expect(1).toBeLikeKomeDayu(2);
+test("toBe", () => {
+  expect(1).toBe(2);
 });
 
-test("toBeLikeKomeDayu not", () => {
-  expect(1).not.toBeLikeKomeDayu(1);
+test("toBe not", () => {
+  expect(1).not.toBe(1);
+});
+
+test("toEqual", () => {
+  expect({ a: 1 }).toEqual({ a: 2 });
+});
+
+test("toEqual not", () => {
+  expect({ a: 1 }).not.toEqual({ a: 1 });
+});
+
+test("toBeNull", () => {
+  expect(1).toBeNull();
+});
+
+test("toBeNull not", () => {
+  expect(null).not.toBeNull();
+});
+
+test("toBeTruthy", () => {
+  expect(false).toBeTruthy();
+});
+
+test("toBeTruthy not", () => {
+  expect(true).not.toBeTruthy();
+});
+
+test("toBeFalsy", () => {
+  expect(true).toBeFalsy();
+});
+
+test("toBeFalsy not", () => {
+  expect(false).not.toBeFalsy();
+});
+
+test("toContain", () => {
+  expect(["a", "b", "c"]).toContain("d");
+});
+
+test("toContain not", () => {
+  expect(["a", "b", "c"]).not.toContain("a");
+});
+
+test("toThrow", () => {
+  expect(() => {
+    return;
+  }).toThrow();
+});
+
+test("toThrow not", () => {
+  expect(() => {
+    throw new Error();
+  }).not.toThrow();
+});
+
+test("CustomError", () => {
+  class CustomError extends Error {
+    constructor(message: string) {
+      super(message);
+      this.name = "CustomError";
+    }
+  }
+
+  expect(() => {
+    throw new Error("一般的なエラー");
+  }).toThrow(new CustomError("特定のエラー"));
 });
--- test/setup.ts
+++ test/setup.ts
@@ -1,20 +1,213 @@
 import { expect } from "vitest";
 
 expect.extend({
-  toBeLikeKomeDayu(received, expected) {
-    const { isNot } = this;
+  toBe(received, expected) {
+    const { isNot, utils } = this;
     return {
       pass: received === expected,
       message: () =>
-        `チャンチャカチャンチャンチャチャンチャチャンチャン\n${expected}${
-          isNot ? "ではない" : ""
-        }かと思ったら～、${received} でした～。チクショー！！`,
+        `${utils.matcherHint("toBe", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャンチャンチャンチャン
+チャンチャカチャンチャンチャンチャンチャンチャン
+
+${utils.printExpected(expected)} ${isNot ? "ではない" : ""}かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      expected,
+      received,
+    };
+  },
+
+  toEqual(received, expected) {
+    const { isNot, utils } = this;
+    return {
+      pass: this.equals(received, expected),
+      message: () =>
+        `${utils.matcherHint("toEqual", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${utils.printExpected(expected)} と一致${
+          isNot ? "しない" : "する"
+        }かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      expected,
+      received,
+    };
+  },
+
+  toBeNull(received) {
+    const { isNot, utils } = this;
+    return {
+      pass: received === null,
+      message: () =>
+        `${utils.matcherHint("toBeNull", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${utils.printExpected(null)} ${isNot ? "ではない" : ""}かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      expected: null,
+      received,
+    };
+  },
+
+  toBeTruthy(received) {
+    const { isNot, utils } = this;
+    return {
+      pass: !!received,
+      message: () =>
+        `${utils.matcherHint("toBeTruthy", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${utils.printExpected(true)} ${isNot ? "ではない" : ""}かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      received,
+    };
+  },
+
+  toBeFalsy(received) {
+    const { isNot, utils } = this;
+    return {
+      pass: !received,
+      message: () =>
+        `${utils.matcherHint("toBeFalsy", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${utils.printExpected(false)} ${isNot ? "ではない" : ""}かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      received,
+    };
+  },
+
+  toContain(received, expected) {
+    const { isNot, utils } = this;
+    const pass =
+      Array.isArray(received) || typeof received === "string"
+        ? received.includes(expected)
+        : false;
+
+    return {
+      pass,
+      message: () =>
+        `${utils.matcherHint("toContain", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${utils.printExpected(expected)} ${
+          isNot ? "を含まない" : "を含む"
+        }かと思ったら～、
+
+${utils.printReceived(received)} でした～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      expected,
+      received,
+    };
+  },
+
+  toThrow(received, expected) {
+    const { isNot, utils } = this;
+
+    if (typeof received !== "function") {
+      throw new Error(
+        `${utils.RECEIVED_COLOR(
+          "received"
+        )} 値は関数でなければなりません。受け取った値: ${utils.printReceived(
+          received
+        )}`
+      );
+    }
+
+    let thrown = false;
+    let error;
+
+    try {
+      received();
+    } catch (e) {
+      thrown = true;
+      error = e;
+    }
+
+    const pass =
+      thrown && (expected === undefined || this.equals(error, expected));
+
+    return {
+      pass,
+      message: () =>
+        `${utils.matcherHint("toThrow", undefined, undefined, {
+          isNot,
+        })}
+
+チャンチャカチャンチャンチャチャンチャチャンチャン
+チャンチャカチャンチャンチャチャンチャチャンチャン
+
+${
+  expected ? utils.printExpected(expected) + "のエラーが" : "エラーが"
+}投げられ${isNot ? "ない" : "る"}かと思ったら～、
+
+${
+  thrown
+    ? `${utils.printReceived(error)}が投げられました`
+    : "何も起きませんでした"
+} ～。
+
+${utils.BOLD_WEIGHT("チクショー！！")}
+`,
+      expected,
+      received,
     };
   },
 });
 
 interface CustomMatchers<R = unknown, T = unknown> {
-  toBeLikeKomeDayu: (expected: T) => R;
+  toBe: (expected: T) => R;
+  toEqual: (expected: T) => R;
+  toBeNull: () => R;
+  toBeTruthy: () => R;
+  toBeFalsy: () => R;
+  toContain: (expected: any) => R;
+  toThrow: (expected?: any) => R;
 }
 
 declare module "vitest" {
